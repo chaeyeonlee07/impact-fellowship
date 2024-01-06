5f99a40525fc235fd9c1138843eb64f3be0ede91
chaeyeonlee07 <lcy2507@naver.com>
2024-01-05 23:51:15 -0800

    figuring out api

---
+++ lib/models/webtoon.dart
@@ -0,0 +1,3 @@
+class WebtoonModel () {
+  
+}
---
+++ lib/services/api_service.dart
@@ -0,0 +1,61 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:geolocator/geolocator.dart';
+
+class Species {
+  final String name;
+  final String imageUrl;
+
+  Species({required this.name, required this.imageUrl});
+}
+
+class ApiService {
+  Future<Position> _determinePosition() async {
+    try {
+      return await Geolocator.getCurrentPosition(
+          desiredAccuracy: LocationAccuracy.high);
+    } catch (e) {
+      print('Error determining position: $e');
+      rethrow;
+    }
+  }
+
+  Future<void> getSpeciesInArea(
+      double minLat, double minLon, double maxLat, double maxLon) async {
+    try {
+      final apiUrl = _buildApiUrl(minLat, minLon, maxLat, maxLon);
+      final response = await http.get(Uri.parse(apiUrl));
+
+      if (response.statusCode == 200) {
+        final data = json.decode(response.body);
+        // Parse and handle the data here
+        print(data);
+      } else {
+        print('Failed to fetch species data: ${response.statusCode}');
+      }
+    } catch (e) {
+      print('Error fetching species data: $e');
+      rethrow;
+    }
+  }
+
+  Future<void> executeApiService() async {
+    try {
+      final position = await _determinePosition();
+      await getSpeciesInArea(
+        position.latitude - 0.02,
+        position.longitude - 0.02,
+        position.latitude + 0.02,
+        position.longitude + 0.02,
+      );
+    } catch (e) {
+      print('Error executing API service: $e');
+      // Handle or rethrow the error based on your requirements
+    }
+  }
+
+  String _buildApiUrl(
+      double minLat, double minLon, double maxLat, double maxLon) {
+    return 'https://api.gbif.org/v1/occurrence/search?geometry=POLYGON(($minLon $minLat, $maxLon $minLat, $maxLon $maxLat, $minLon $maxLat, $minLon $minLat))';
+  }
+}
