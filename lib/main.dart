d2cbb4b98a279f509211d9bd7aaea7353e008cb9
chaeyeonlee07 <lcy2507@naver.com>
2024-01-06 15:14:23 -0800

    challenge of the day ui

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: challengeToday(),
+      home: ChallengeToday(),
     );
   }
 }
