d417a17a7e09b4b3ec2630d005842f0c0adb9bd0
Yunsu Ha <66456341+yuha1126@users.noreply.github.com>
2024-01-06 22:50:05 -0800

    changed main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:impact/screens/camera.dart';
 import 'package:impact/services/api_service.dart';
 import 'package:impact/screens/onboarding1.dart';
 import 'package:impact/screens/challengeToday.dart';
-import 'package:impact/screens/directoryOverlay.dart';
+import 'package:impact/screens/challengeToday.dart';
+import 'package:impact/screens/directoryoverlay.dart';
+import 'package:camera/camera.dart';
 
-void main() {
+
+
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  // ApiService().executeApiService();
-  runApp(const App());
+  final cameras = await availableCameras();
+  runApp(App(cameras : cameras));
 }
 
 class App extends StatelessWidget {
-  const App({Key? key}) : super(key: key);
-
+  // const App({Key? key}) : super(key: key);
+  final List<CameraDescription> cameras;
+  const App({Key? key, required this.cameras}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: DirectoryOverlay(),
+      home: DirectoryOverlay(), // replace with CameraScreen(cameras: cameras) to see camera page
     );
   }
 }
