7bb3c2657003cf0bc6c4a26303b8187364055668
Yunsu Ha <66456341+yuha1126@users.noreply.github.com>
2024-01-07 00:36:11 -0800

    added camera screen to overlay

--- lib/screens/directoryOverlay.dart
+++ lib/screens/directoryOverlay.dart
@@ -2,37 +2,91 @@ import 'package:flutter/material.dart';
 import 'package:impact/screens/challengeToday.dart';
 import 'package:impact/screens/profilepage.dart';
 import 'package:impact/screens/calendar.dart';
+import 'package:impact/screens/camera.dart';
+import 'package:camera/camera.dart';
 
 
-class DirectoryOverlay extends StatelessWidget {
+class DirectoryOverlay extends StatefulWidget {
   const DirectoryOverlay({super.key});
 
   @override
+  _DirectoryOverlayState createState() => _DirectoryOverlayState();
+}
+
+class _DirectoryOverlayState extends State<DirectoryOverlay> {
+  List<CameraDescription>? camerasList;
+
+  @override
+  void initState() {
+    super.initState();
+    initializeCameras();
+  }
+
+  Future<void> initializeCameras() async {
+    camerasList = await availableCameras();
+    setState(() {}); // Trigger a rebuild after initializing cameras
+  }
+
+  @override
   Widget build(BuildContext context) {
+    // Check if camerasList is null or empty before building the UI
+    if (camerasList == null) {
+      return MaterialApp(
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const Scaffold(
+          body: Center(
+            child: CircularProgressIndicator(),
+          ),
+        ),
+      );
+    }
+
     return MaterialApp(
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const BottomNavigationOverlay(),
+      home: BottomNavigationOverlay(camerasList: camerasList),
     );
   }
 }
 
+
+
 class BottomNavigationOverlay extends StatefulWidget {
-  const BottomNavigationOverlay({super.key});
+  final List<CameraDescription>? camerasList;
+
+  const BottomNavigationOverlay({
+    Key? key,
+    required this.camerasList,
+  }) : super(key: key);
 
   @override
-  _BottomNavigationOverlayState createState() => _BottomNavigationOverlayState();
+  _BottomNavigationOverlayState createState() =>
+      _BottomNavigationOverlayState();
 }
 
 class _BottomNavigationOverlayState extends State<BottomNavigationOverlay> {
-  int _currentIndex = 0;
+  late int _currentIndex;
+  late List<Widget> _screens;
 
-  final List<Widget> _screens = [
-    const ProfilePage(),
-    ChallengeToday(),
-    const Calendars(),
-  ];
+  @override
+  void initState() {
+    super.initState();
+    _currentIndex = 0;
+    _initializeScreens();
+  }
+
+  void _initializeScreens() {
+    _screens = [
+      const ProfilePage(),
+      ChallengeToday(),
+      if (widget.camerasList != null)
+        CameraScreen(cameras: widget.camerasList!),
+      const Calendars(),
+    ];
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -40,32 +94,31 @@ class _BottomNavigationOverlayState extends State<BottomNavigationOverlay> {
       body: _screens[_currentIndex],
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: _currentIndex,
+        fixedColor: const Color.fromARGB(255, 33, 226, 39),
+        unselectedItemColor: const Color.fromARGB(159, 0, 97, 24),  
         onTap: (index) {
           setState(() {
             _currentIndex = index;
           });
         },
-        items: const [
-          BottomNavigationBarItem(
+        items: [
+          const BottomNavigationBarItem(
             icon: Icon(Icons.home),
             label: 'Profile',
           ),
-          BottomNavigationBarItem(
+          const BottomNavigationBarItem(
             icon: Icon(Icons.school),
             label: 'Challenge of Day',
           ),
-          // BottomNavigationBarItem(
-          //   icon: Icon(Icons.camera_enhance_outlined),
-          //   label: 'Camera',
-          // ),
-          BottomNavigationBarItem(
+          if (widget.camerasList != null)
+            const BottomNavigationBarItem(
+              icon: Icon(Icons.camera_enhance_outlined),
+              label: 'Camera',
+            ),
+          const BottomNavigationBarItem(
             icon: Icon(Icons.business),
             label: 'Calendar',
           ),
-          // BottomNavigationBarItem(
-          //   icon: Icon(Icons.school),
-          //   label: 'Friends',
-          // ),
         ],
       ),
     );
