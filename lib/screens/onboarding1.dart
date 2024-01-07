1c109f33fce7ddc22788876418e7450a97049f94
Yunsu Ha <66456341+yuha1126@users.noreply.github.com>
2024-01-07 11:29:46 -0800

    added onboarding on startup

--- lib/screens/onboarding1.dart
+++ lib/screens/onboarding1.dart
@@ -1,7 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:impact/screens/directoryoverlay.dart';
 
-class OnBoarding1 extends StatelessWidget {
-  const OnBoarding1({Key? key}) : super(key: key);
+
+class OnBoarding1 extends StatefulWidget {
+  const OnBoarding1({super.key});
+
+
+  @override
+  _OnBoarding1 createState() => _OnBoarding1();
+}
+
+class _OnBoarding1 extends State<OnBoarding1> {
+
+  // Simulate a delay of 3 seconds
+  Future<void> _mockSplashScreen() async {
+    await Future.delayed(const Duration(seconds: 3));
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    _mockSplashScreen().then(
+      (_) {
+        // After the splash screen, navigate to the main page
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => const DirectoryOverlay()),
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
